63106fcc55f23d60171b0fd8b3f74d52f61e154e
Ray Donnelly <mingw.android@gmail.com>
2018-07-03 16:12:43 -0400

    Get rid of stdlib=libc++ and make in parallel, verbosely

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-if [ "$(uname)" == "Darwin" ]
-then
-  export CXX="${CXX} -stdlib=libc++"
-fi
-
 ./configure --prefix=$PREFIX --with-icu --disable-network
-make
+make -j${CPU_COUNT} ${VERBOSE_AT}
 make check
 make install
